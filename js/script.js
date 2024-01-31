4b7304d5de3b27e193cad587b11a8daac319b2da
Jonatas Pessoa <brito_jonatas@hotmail.com>
2024-01-31 04:08:14 -0300

    Correção de layout responsivo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
 $('#slider').nivoSlider();
+
+
+function showMenu() {
+    let menuMobile = document.querySelector('.menu-mobile');
+    if(menuMobile.classList.contains('openMenu')) {
+        menuMobile.classList.remove('openMenu');
+        document.querySelector('.icon').src = 'assets/menu-mobile.png';
+    } else {
+        menuMobile.classList.add('openMenu');
+        document.querySelector('.icon').src = 'assets/close-menu--mobile.png';s
+    }
+};
+
+
+function showSubMenu() {
+    let menuMobile = document.querySelector('.subMobile');
+    if(menuMobile.classList.contains('openMenu')) {
+        menuMobile.classList.remove('openMenu');
+        document.querySelector('.icon').src = 'assets/menu-mobile.png';
+    } else {
+        menuMobile.classList.add('openMenu');
+        document.querySelector('.icon').src = 'assets/close-menu--mobile.png';s
+    }
+};
+
+function showSubMenu() {
+    let menuMobile = document.querySelector('.subMobile');
+    if(menuMobile.classList.contains('openMenu')) {
+        menuMobile.classList.remove('openMenu');
+    } else {
+        menuMobile.classList.add('openMenu');
+    }
+};
+
+function showSubMenu2() {
+    let menuMobile = document.querySelector('.subMobile2');
+    if(menuMobile.classList.contains('openMenu')) {
+        menuMobile.classList.remove('openMenu');
+    } else {
+        menuMobile.classList.add('openMenu');
+    }
+};
+
+
+
